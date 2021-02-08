e6da97ed7f4a81ee9904fb80c75bfcb84d4c9e97
lnewsom <lara@laranewsom.com>
2021-02-07 21:00:37 -0600

    1 file modified

--- index.ts
+++ index.ts
@@ -1,18 +1,34 @@
-import { from } from 'rxjs'; 
-import { map } from 'rxjs/operators';
+import { BehaviorSubject, from, interval, Observable,  of,  Subject, zip } from 'rxjs'; 
+import { concatMap, delay, map, scan, tap, take, windowCount, mergeAll, bufferCount } from 'rxjs/operators';
 
 const LIMIT = 100;
 const BATCH_SIZE = 10;
-const sourceArray = [];
 let index = 0;
+const incomingSubject = new Subject();
 
-while (index < LIMIT) {
-  index++;
-  sourceArray.push({key: index});
-}
 
-const source = from(sourceArray).pipe(
-  map(x => `Hello ${x.key}!`)
+//const source = incomingSubject.pipe(
+//  bufferCount(10),
+//  concatMap((win) => from(win).pipe(
+//      delay(3000),
+//      concatMap(x => { 
+//          return of(x).pipe(delay(1000))
+ //       }))));
+
+const source = incomingSubject.pipe(
+  bufferCount(10),
+  concatMap((win) => from(win).pipe(
+      delay(100),
+      concatMap(x => { 
+          return of(x).pipe(delay(10))
+        }
+      )
+  ))
 );
 
 source.subscribe(console.log);
+
+while (index < LIMIT) {
+  index++;
+  incomingSubject.next({key: index});
+}
