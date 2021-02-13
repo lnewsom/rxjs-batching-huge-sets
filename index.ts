ffd7468fd9711dd8361793f69dd06cc1d77c9049
lnewsom <lara@laranewsom.com>
2021-02-13 15:54:27 -0600

    adding mergemap examples

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-import { of, from } from 'rxjs'; 
-import { map } from 'rxjs/operators';
+import { of, from, Subject } from 'rxjs'; 
+import { map, concatMap, take, mergeMap } from 'rxjs/operators';
 
 const delayValues = from([
   { key: 1, source: "delay" },
@@ -10,8 +10,39 @@ const delayValues = from([
   { key: 6, source: "delay" }
 ]);
 
-const source = of('World').pipe(
-  map(x => `Hello ${x}!`)
+const neverEndingObservable = new Subject();
+
+setInterval(() => neverEndingObservable.next(10), 500);
+
+const source = delayValues.pipe(
+  concatMap(x => {
+    return neverEndingObservable.pipe(
+      map((y: number) => y * x.key),
+      take(3)
+    );
+  })
 );
 
-source.subscribe(console.log);
+//source.subscribe(console.log);
+
+const mergeMapSource = delayValues.pipe(
+  mergeMap(x => {
+    return neverEndingObservable.pipe(
+      map((y: number) => y * x.key),
+      take(3)
+    );
+  })
+);
+
+//mergeMapSource.subscribe(console.log);
+
+const mergeMapSourceAsConcatMap = delayValues.pipe(
+  mergeMap(x => {
+    return neverEndingObservable.pipe(
+      map((y: number) => y * x.key),
+      take(3)
+    );
+  }, 1)
+);
+
+mergeMapSourceAsConcatMap.subscribe(console.log);
